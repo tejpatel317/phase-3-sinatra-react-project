6b83d1858bdae72820ec552e5f6c28da1ea6917b
Tej Patel <tejpatel317@gmail.com>
2022-10-03 19:03:45 -0400

    Creates table for users migration

--- db/migrate/20221003223629_create_projects.rb
+++ db/migrate/20221003223629_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :details
+      t.string :detail
       t.date :due_date
     end
   end
--- db/migrate/20221003223706_create_users.rb
+++ db/migrate/20221003223706_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :logo
+      t.integer :employee_id
+      t.integer :project_id
+    end
   end
 end
