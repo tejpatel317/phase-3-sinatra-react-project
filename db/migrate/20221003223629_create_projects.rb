d4e6d2565aef6aae393dab8561757a7253bfc448
Tej Patel <tejpatel317@gmail.com>
2022-10-03 18:57:38 -0400

    Creates table for projects migration

--- db/migrate/20221003223629_create_projects.rb
+++ db/migrate/20221003223629_create_projects.rb
@@ -1,4 +1,9 @@
 class CreateProjects < ActiveRecord::Migration[6.1]
   def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :details
+      t.date :due_date
+    end
   end
 end
