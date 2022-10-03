f619318fc2c1575aa615724100cbb9013fb8f8d3
Tej Patel <tejpatel317@gmail.com>
2022-10-03 18:43:28 -0400

    Creates table for employees migration

--- db/migrate/20221003223554_create_employees.rb
+++ db/migrate/20221003223554_create_employees.rb
@@ -1,4 +1,10 @@
 class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :position
+      t.string :avatar
+    end
   end
 end
