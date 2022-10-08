ac237318170010f09c644efb9ad76605d727faea
Tej Patel <tejpatel317@gmail.com>
2022-10-08 12:23:34 -0400

    Adds GET request to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/projects" do
+    projects = Project.all.order(:due_date)
+    projects.to_json(include: :employees)
+  end
+
+  get "/employees" do
+    employees = Employee.all.order(:first_name)
+    employees.to_json(include: :projects)
   end
 
 end
