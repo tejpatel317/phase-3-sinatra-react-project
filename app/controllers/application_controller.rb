2ac5fe4b424b8fa40a14bf5c2c8b6f76186484b8
Tej Patel <tejpatel317@gmail.com>
2022-10-08 17:52:22 -0400

    Completes projects POST request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,21 @@ class ApplicationController < Sinatra::Base
     employees.to_json(include: :projects)
   end
 
+  post "/projects" do
+    project = Project.create(
+      name: params[:name],
+      detail: params[:detail],
+      due_date: params[:due_date]
+    )
+
+    params[:project_employees].each do |e|
+      Assignment.create(
+        project_id: project.id,
+        employee_id: e
+      )
+    end
+
+    project.to_json(include: :employees)
+  end
+
 end
